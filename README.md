f741365aa07c6d9a1be13c9378a99cd9951c11fb
flxndn <felix.anadon@gmail.com>
2015-02-25 18:39:56 +0100

    - References cleaned - corrected svg styles - make a group with the cardbox - fixed bug. the hole was not erased

--- README.md
+++ README.md
@@ -3,15 +3,13 @@ Customizable clambox for book protecting in SVG
 
 
 ## References
+### Clamshell box
 *  [The 5-minute Phase Box - part 1 - Uppsala University Library](https://www.youtube.com/watch?v=EDzZFUlHd0s)
 
-*  [Using Javascript to control an SVG](http://www.petercollingridge.co.uk/data-visualisation/using-javascript-control-svg) Peter's Website
-
-*  [Inter-document scripting: referencing embedded SVG](https://developer.mozilla.org/en-US/docs/Web/SVG/Scripting)
-
-*  [SVG Web library](http://dev.w3.org/SVG/tools/svgweb/docs/UserManual.html)
-
+### Javascript + SVG
 *  [Snap.svg](http://snapsvg.io/)
+
+*  [Snap.svg Tutorial](http://svg.dabbles.info/)
 
 ## Done
 *  Using Snap.svg
--- clamshellbox.js
+++ clamshellbox.js
@@ -2,7 +2,7 @@
 //------------------------------------------------------------------------------
 function clear(snap) {
 //------------------------------------------------------------------------------
-	ee=snap.selectAll('.cardboard');
+	ee=snap.selectAll('.cardboard', '.hole');
 	for(i=ee.length-1;i>=0; i--) {
 		ee[i].remove()
 	}
@@ -10,36 +10,31 @@ function clear(snap) {
 //------------------------------------------------------------------------------
 function rectadapt(snap, id, x, y, w, h) {
 //------------------------------------------------------------------------------
-	var r1 = snap.rect(x, y, w, h);
-	r1.addClass('cardboard');
-	r1.attr({
-		fill: "#bada55",
-		stroke: "#000",
-		strokeWidth: 2
-	});
+	var r = snap.rect(x, y, w, h);
+	r.addClass('cardboard');
+	return r;
 }
 //------------------------------------------------------------------------------
 function adapt (snap, height, width, thick, paper) {
 //------------------------------------------------------------------------------
 	clear(snap);
-	rectadapt(snap, 'up_right', 3*thick+width, 0, width+paper+2*thick, thick); 
-	rectadapt(snap, 'base_front', 3*thick+width, thick, width+paper, height+2*paper); 
-	rectadapt(snap, 'down_right', 3*thick+width, thick+height+2*paper, width+paper+2*thick, thick); 
-	rectadapt(snap, 'right', 3*thick+width*2+paper, thick, thick, height+2*paper); 
-	rectadapt(snap, 'right_right', 4*thick+width*2+paper, thick, thick, height+2*paper); 
-	rectadapt(snap, 'hinge', 2*thick+width, thick+paper, thick, height); 
-	rectadapt(snap, 'base_back', 2*thick, thick+paper, width, height); 
-	rectadapt(snap, 'left', thick, thick+paper, thick, height); 
-	rectadapt(snap, 'left_left', 0, thick+paper, thick, height); 
-	rectadapt(snap, 'up_left', 0, paper, width+3*thick, thick); 
-	rectadapt(snap, 'down_left', 0, thick+height+paper, width+3*thick, thick);
+	g = snap.g();
+	g.add(rectadapt(snap, 'up_right', 3*thick+width, 0, width+paper+2*thick, thick));
+	g.add(rectadapt(snap, 'base_front', 3*thick+width, thick, width+paper, height+2*paper)); 
+	g.add(rectadapt(snap, 'down_right', 3*thick+width, thick+height+2*paper, width+paper+2*thick, thick)); 
+	g.add(rectadapt(snap, 'right', 3*thick+width*2+paper, thick, thick, height+2*paper)); 
+	g.add(rectadapt(snap, 'right_right', 4*thick+width*2+paper, thick, thick, height+2*paper)); 
+	g.add(rectadapt(snap, 'hinge', 2*thick+width, thick+paper, thick, height)); 
+	g.add(rectadapt(snap, 'base_back', 2*thick, thick+paper, width, height)); 
+	g.add(rectadapt(snap, 'left', thick, thick+paper, thick, height)); 
+	g.add(rectadapt(snap, 'left_left', 0, thick+paper, thick, height)); 
+	g.add(rectadapt(snap, 'up_left', 0, paper, width+3*thick, thick)); 
+	g.add(rectadapt(snap, 'down_left', 0, thick+height+paper, width+3*thick, thick));
 
 	var hole = snap.circle(4*thick+width*2+paper, thick+(height+2*paper)/2, 12.7 );
-	hole.attr({
-		fill: "#fff",
-		stroke: "#000",
-		strokeWidth: 1
-	});
+	hole.addClass('hole');
+	g.add(hole)
+	return g;
 }
 //------------------------------------------------------------------------------
 function update() {
